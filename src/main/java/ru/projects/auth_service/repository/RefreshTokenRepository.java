db7a7ea6ec139d9b1204f7e402f0ecf99060a06b
AxTerium <kirill.kazancev.2001@mail.ru>
2025-07-09 16:53:37 +0700

    uuid

--- src/main/java/ru/projects/auth_service/repository/RefreshTokenRepository.java
+++ src/main/java/ru/projects/auth_service/repository/RefreshTokenRepository.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Repository;
 import ru.projects.auth_service.model.RefreshToken;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByToken(String token);
 }
--- src/main/java/ru/projects/auth_service/repository/RoleRepositiry.java
+++ src/main/java/ru/projects/auth_service/repository/RoleRepositiry.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Repository;
 import ru.projects.auth_service.model.Role;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface RoleRepositiry extends JpaRepository<Role, Long> {
+public interface RoleRepositiry extends JpaRepository<Role, UUID> {
     Optional<Role> findByName(String name);
 }
